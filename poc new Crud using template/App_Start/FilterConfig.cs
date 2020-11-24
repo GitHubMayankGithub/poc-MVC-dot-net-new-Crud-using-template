1659c2c53b5179decd0741b0d30260fe2f1988d3
GitHubMayankGithub <explorermayank94@gmail.com>
2020-11-24 15:02:33 +0530

    Add project files.

---
+++ poc new Crud using template/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace poc_new_Crud_using_template
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ poc new Crud using template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="poc_new_Crud_using_template.MvcApplication" Language="C#" %>
